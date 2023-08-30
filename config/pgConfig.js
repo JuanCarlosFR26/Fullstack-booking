3023c78fe6d2cbe38d951c90b7077646f7576680
unknown <jcf.daw@gmail.com>
2023-08-30 18:55:29 +0200

    changing pgconfig

--- config/pgConfig.js
+++ config/pgConfig.js
@@ -1,11 +1,7 @@
 require('dotenv').config();
 const Pool = require('pg').Pool;
 const pool = new Pool({
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    password: `${process.env.PASSWORD}`,
-    port: process.env.DATABASE_PORT
+    connectionString: process.env.POSTGRESS_URL
 });
 
 module.exports = {
